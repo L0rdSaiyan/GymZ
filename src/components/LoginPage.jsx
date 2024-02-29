61b456533c3fb454f8c27a1c000f8164ec2d5fb1
Victor Sales <thehoodgamer20@gmail.com>
2024-02-29 19:26:35 -0300

    adicionado a camada de model para o usuário

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,38 +1,48 @@
+import { func } from "prop-types";
 import styles from "./LoginPage.module.css";
-import { useState } from "react";
+import User from "../model/UserModel";
+import { useState, useEffect } from "react";
 
 export default function LoginPage() {
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState({});
   const [name, setName] = useState("");
   const [pass, setPass] = useState("");
 
-  function handleNameChange(event) {
+  function handleNameChange(event) 
+  {
     setName(event.target.value);
     console.log(name);
+    console.log(user.name)
   }
 
-  function handlePassChange(event) {
+  function handlePassChange(event) 
+  {
     setPass(event.target.value);
     console.log(pass);
   }
 
-  function getUser(name, password) {
-    fetch(`http://localhost:5000/users?name=${name}&password=${password}`, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-      },
+  function getUser(name, pass)
+  {
+    fetch(`http://localhost:5000/users?name=${name}&password=${pass}`,
+    {
+      method:"GET",
+      headers:
+      {
+        "Content-type": "application/json"
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        setUser(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching user data:", error);
-      });
+    .then((response) => response.json())
+    .then((data)=>{
+        console.log(data[0].id)
+        const newUser = new User(
+        data[0].id,
+        data[0].name,
+        data[0].password,
+        data[0].content
+        )
+        setUser(newUser)
+    } )
   }
-
 
   return (
     <div className={styles.container}>
@@ -60,10 +70,10 @@ export default function LoginPage() {
             </button>
           </div>
           <div>
-          <p>{user[0].exercices[0].name}</p>
+            
           </div>
         </div>
-        <footer>Footer</footer>
+        <footer> &copy; {new Date().getFullYear()} </footer>
       </div>
     </div>
   );
---
+++ src/model/UserModel.js
@@ -0,0 +1,12 @@
+class User
+{
+    constructor(id, name, password, content)
+    {
+        this.id = id
+        this.name = name
+        this.password = password
+        this.content = content
+    }
+}
+
+export default User
